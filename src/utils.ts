33003e7274e5226494a393143f1bcfe3447307b9
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-10-03 23:22:25 +0300

    implement markdown utils

---
+++ src/utils.ts
@@ -0,0 +1,21 @@
+export function getFrontmatterLengthInLines(body: string) {
+    let yamlSymbol = "---";
+    if (!body.startsWith(yamlSymbol)) {
+        return 0;
+    }
+    let yamlEndSymbolIndex = body.indexOf(yamlSymbol, 3);
+    let yamlEndIndex = body.indexOf("\n", yamlEndSymbolIndex);
+    let frontmatter = body.substring(0, yamlEndIndex);
+    return frontmatter.split("\n").length;
+}
+
+export function getFirstSignificantLineNumber(body: string) {
+    let fmLength = getFrontmatterLengthInLines(body);
+    let lines = body.split("\n");
+    for (let i = fmLength; i < lines.length; i++) {
+        if (lines[i].trim()) {
+            return i;
+        }
+    }
+    return lines.length;
+}
