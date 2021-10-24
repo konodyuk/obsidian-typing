5fe0811f96c2cc880b0642ee647b961225b62e63
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-10-24 15:35:08 +0300

    reimplement Type and TypeRegistry

--- src/type.ts
+++ src/type.ts
@@ -1,50 +1,82 @@
-import { Notice, TFile, MarkdownRenderer } from "obsidian";
-import TypingPlugin from "./main";
-import {
-    MarginalSpec,
-    IconSpec,
-    Config,
-    RenderingSpec,
-    TextSpec,
-} from "./config";
-import { ScriptContext } from "./eval";
-import { LiteralValue } from "obsidian-dataview";
+import { TypeSpec, OverrideSpec } from "./config_specs";
+import { EvalContext } from "./eval";
 import { Field } from "./field";
+import { Config, Marginal, Rendering, Action } from "./config";
+import { TypedNote } from "./typed_note";
+import { autoFieldAccessor } from "./field_accessor";
+import { Prefix } from "./prefix";
+import { gracefullyAlert } from "./utils";
 
-export class Type {
-    constructor(
-        public registry: TypeRegistry,
-        public name: string,
-        public folder: string,
-        public parents: Array<Type>,
-        public fields?: Array<Field>,
-        public header?: MarginalSpec,
-        public footer?: MarginalSpec,
-        public icon?: IconSpec,
-        public rendering?: RenderingSpec
-    ) {}
-    consolidateProperties() {
-        let fields: Array<Field> = [];
-        for (let parent of this.parents) {
-            if (!this.header && parent.header) {
-                this.header = parent.header;
-            }
-            if (!this.footer && parent.footer) {
-                this.footer = parent.footer;
-            }
-            if (parent.fields) {
-                fields.push(...parent.fields);
+export class StaticTypeAttributesMixin {
+    header: Marginal;
+    footer: Marginal;
+    icon: string;
+    render: Rendering;
+    prefix: Prefix;
+}
+
+export class Type extends StaticTypeAttributesMixin {
+    conf: Config;
+    name: string;
+    folder: string;
+    fields: Array<Field>;
+    actions: Array<Action>;
+    prefix: Prefix;
+    createable: boolean;
+
+    static async fromSpec(spec: TypeSpec, conf: Config): Promise<Type> {
+        let result = new this();
+        result.conf = conf;
+        result.name = spec.name;
+        result.folder = spec.folder;
+
+        result.fields = [];
+        if (spec.parents != null) {
+            for (let parentName of spec.parents) {
+                let parent = conf.types[parentName];
+                if (parent == null) {
+                    continue;
+                }
+                if (parent.header) {
+                    result.header = parent.header;
+                }
+                if (parent.footer) {
+                    result.footer = parent.footer;
+                }
+                if (parent.fields) {
+                    result.fields.push(...parent.fields);
+                }
             }
         }
-        if (this.fields) {
-            fields.push(...this.fields);
+        if (spec.header != null) {
+            result.header = await Marginal.fromSpec(spec.header, conf);
         }
-        this.fields = fields;
+        if (spec.footer != null) {
+            result.footer = await Marginal.fromSpec(spec.footer, conf);
+        }
+        if (spec.fields != null) {
+            result.fields.push(...spec.fields);
+        }
+        if (spec.icon != null) {
+            result.icon = spec.icon;
+        }
+        if (spec.render != null) {
+            result.render = await Rendering.fromSpec(spec.render, conf);
+        }
+        if (spec.prefix != null) {
+            result.prefix = Prefix.fromString(spec.prefix);
+        }
+        result.createable = true;
+        if (spec.createable != null) {
+            result.createable = spec.createable;
+        }
+        return result;
     }
-    createNewFile(): Promise<TFile> {
-        let vault = this.registry.plugin.app.vault;
+
+    async new(fields: { [name: string]: string }) {
+        let vault = this.conf.plugin.app.vault;
         if (!vault.getAbstractFileByPath(this.folder)) {
-            vault.createFolder(this.folder);
+            await vault.createFolder(this.folder);
         }
         let serial = 0;
         while (
@@ -52,46 +84,14 @@ export class Type {
         ) {
             serial++;
         }
-        return vault.create(this.folder + `/Untitled${serial}.md`, "");
-    }
-    // TODO: use render(mode=link) as a generic substitution of typeIconPostProcessor
-    async render(
-        page: Record<string, LiteralValue>,
-        mode: "link" | "card",
-        containerEl: HTMLElement
-    ) {
-        if (
-            !this.rendering ||
-            (mode == "link" && !this.rendering.link) ||
-            (mode == "card" && !this.rendering.card)
-        ) {
-            let tempContainerEl = document.createElement("div");
-            MarkdownRenderer.renderMarkdown(
-                `[[${page.file.path}|${page.file.name}]]`,
-                tempContainerEl,
-                "",
-                null
-            );
-            containerEl.appendChild(tempContainerEl.firstChild.firstChild);
-            tempContainerEl.removeChild(tempContainerEl.firstChild);
-            return;
-        }
-
-        let scriptContext = new ScriptContext(
-            await this.registry.plugin.dataviewApi(),
-            "",
-            containerEl,
-            this,
-            page
-        );
-
-        if (mode == "link") {
-            return scriptContext.evalScript(this.rendering.link.value);
-        }
-        if (mode == "card") {
-            return scriptContext.evalScript(this.rendering.card.value);
+        let newPath = this.folder + `/Untitled${serial}.md`;
+        await vault.create(newPath, "");
+        let accessor = await autoFieldAccessor(newPath, this.conf.plugin);
+        for (let field in fields) {
+            await accessor.setValue(field, fields[field]);
         }
     }
+
     getFieldOrder(): { [name: string]: number } {
         let result: { [name: string]: number } = {};
         if (!this.fields) {
@@ -104,48 +104,58 @@ export class Type {
     }
 }
 
-class Override {
+export class Override {
     constructor(
-        public registry: TypeRegistry,
+        public conf: Config,
         public condition: string,
-        public header?: MarginalSpec,
-        public footer?: MarginalSpec,
-        public icon?: IconSpec
+        public header?: Marginal,
+        public footer?: Marginal,
+        public icon?: string
     ) {}
-    async check(path: string, type: Type): Promise<boolean> {
-        let scriptContext = new ScriptContext(
-            await this.registry.plugin.dataviewApi(),
-            path,
-            null,
-            type
-        );
+    static async fromSpec(spec: OverrideSpec, conf: Config): Promise<Override> {
+        let header = null;
+        if (spec.header != null) {
+            header = await Marginal.fromSpec(spec.header, conf);
+        }
+
+        let footer = null;
+        if (spec.footer != null) {
+            footer = await Marginal.fromSpec(spec.footer, conf);
+        }
+
+        let condition = spec.condition;
+        if (condition && conf.settings?.preamble) {
+            condition = conf.settings.preamble.source + ";" + condition;
+        }
+
+        return new this(conf, condition, header, footer, spec.icon);
+    }
+    check(note: TypedNote): boolean {
+        let ctx = new EvalContext(this.conf.plugin.getDefaultContext(note));
         try {
-            // TODO: fix overrides aren't checked correctly until DV index is built
-            return scriptContext.evalScript(this.condition);
+            return ctx.eval(this.condition);
         } catch (e) {
             return false;
         }
     }
-    apply(type: Type) {
+    apply(note: TypedNote) {
         if (this.header) {
-            type.header = this.header;
+            note.header = this.header;
         }
         if (this.footer) {
-            type.footer = this.footer;
+            note.footer = this.footer;
         }
         if (this.icon) {
-            type.icon = this.icon;
+            note.icon = this.icon;
         }
     }
 }
 
 export class TypeRegistry {
-    types: { [name: string]: Type } = {};
-    typesList: Array<Type> = [];
-    overrides: Array<Override> = [];
-    folderIndex: { [folder: string]: Type } = {};
-    isEmpty: boolean = true;
-    constructor(public plugin: TypingPlugin) {}
+    public types: { [name: string]: Type } = {};
+    public typesList: Array<Type> = [];
+    public overrides: Array<Override> = [];
+    public folderIndex: { [folder: string]: Type } = {};
     addType(newType: Type) {
         for (let name in this.types) {
             if (name == newType.name) {
@@ -157,164 +167,49 @@ export class TypeRegistry {
                 return;
             }
         }
-        newType.consolidateProperties();
         this.types[newType.name] = newType;
+        this.folderIndex[newType.folder] = newType;
+        this.typesList = Object.keys(this.types).map(
+            (name) => this.types[name]
+        );
+    }
+    addOverride(override: Override) {
+        this.overrides.push(override);
+    }
+    getTypeByName(name: string): Type {
+        return this.types[name];
+    }
+    getTypeByFolder(folder: string): Type | null {
+        let type = this.folderIndex[folder];
+        if (!type) {
+            return null;
+        }
+        return type;
+    }
+    getTypeByPath(path: string): Type | null {
+        let lastIndexOfPathSep = path.lastIndexOf("/");
+        if (lastIndexOfPathSep == -1) {
+            return null;
+        }
+        let folder = path.substring(0, lastIndexOfPathSep);
+
+        let type = this.getTypeByFolder(folder);
+        return type;
+    }
+    applyOverrides(note: TypedNote): TypedNote {
+        for (let override of this.overrides) {
+            if (override.check(note)) {
+                override.apply(note);
+            }
+        }
+        return note;
     }
     clear() {
-        this.isEmpty = true;
         this.types = {};
         this.typesList = [];
         this.folderIndex = {};
         this.overrides = [];
     }
-    async buildFromConfig(config: Config) {
-        if (!this.isEmpty) {
-            this.clear();
-            this.isEmpty = false;
-        }
-        let preamble = "";
-        if (config.preamble) {
-            preamble = config.preamble + ";\n";
-        }
-        for (let typeSpec of config.types) {
-            let parents = [];
-            if (typeSpec.parents) {
-                for (let parent of typeSpec.parents) {
-                    if (!(parent in this.types)) {
-                        gracefullyAlert("undefined parent: " + parent);
-                        return;
-                    }
-                    parents.push(this.types[parent]);
-                }
-            }
-            let header = typeSpec.header;
-            if (header) {
-                this.ensureCorrectMarginalSpec(header, preamble);
-            }
-            let icon = typeSpec.icon;
-            if (icon) {
-                this.ensureCorrectIcon(icon);
-            }
-            let footer = typeSpec.footer;
-            if (footer) {
-                this.ensureCorrectMarginalSpec(footer, preamble);
-            }
-            let rendering = typeSpec.render;
-            if (rendering) {
-                if (rendering.card) {
-                    this.ensureCorrectTextSpec(rendering.card);
-                }
-                if (rendering.link) {
-                    this.ensureCorrectTextSpec(rendering.link);
-                }
-            }
-            let fields = [];
-            if (typeSpec.fields) {
-                for (let field of typeSpec.fields) {
-                    fields.push(
-                        new Field(
-                            this,
-                            field.name,
-                            field.kind,
-                            field.types,
-                            field.options
-                        )
-                    );
-                }
-            }
-            let type = new Type(
-                this,
-                typeSpec.name,
-                typeSpec.folder,
-                parents,
-                fields,
-                header,
-                footer,
-                icon,
-                rendering
-            );
-            this.addType(type);
-        }
-        for (let name in this.types) {
-            let type = this.types[name];
-            this.folderIndex[type.folder] = type;
-        }
-        this.typesList = Object.keys(this.types).map(
-            (name) => this.types[name]
-        );
-        for (let overrideSpec of config.overrides) {
-            let condition = overrideSpec.condition;
-            condition = preamble + condition;
-
-            let header = overrideSpec.header;
-            if (header) {
-                this.ensureCorrectMarginalSpec(header, preamble);
-            }
-            let icon = overrideSpec.icon;
-            if (icon) {
-                this.ensureCorrectIcon(icon);
-            }
-            let footer = overrideSpec.footer;
-            if (footer) {
-                this.ensureCorrectMarginalSpec(footer, preamble);
-            }
-            let override = new Override(this, condition, header, footer, icon);
-            this.overrides.push(override);
-        }
-    }
-    async getType(path: string): Promise<Type> {
-        let lastIndexOfPathSep = path.lastIndexOf("/");
-        if (lastIndexOfPathSep == -1) {
-            return;
-        }
-
-        let folder = path.substring(0, lastIndexOfPathSep);
-        let type = this.folderIndex[folder];
-        if (!type) {
-            return;
-        }
-        let isCloned = false;
-        for (let override of this.overrides) {
-            if (await override.check(path, type)) {
-                if (!isCloned) {
-                    type = Object.create(type);
-                    isCloned = true;
-                }
-                override.apply(type);
-            }
-        }
-        return type;
-    }
-    async ensureCorrectTextSpec(textSpec: TextSpec) {
-        if (textSpec.value && textSpec.file) {
-            gracefullyAlert(
-                `both value and file are specified in TextSpec: ${textSpec.file}`
-            );
-            textSpec.file = null;
-        }
-        if (textSpec.file) {
-            textSpec.value = await this.plugin.app.vault.adapter.read(
-                textSpec.file
-            );
-        }
-    }
-    async ensureCorrectMarginalSpec(marginal: MarginalSpec, preamble: string) {
-        await this.ensureCorrectTextSpec(marginal);
-        if (!marginal.kind) {
-            marginal.kind = "js";
-        }
-        if (marginal.kind == "js") {
-            marginal.value = preamble + marginal.value;
-        }
-    }
-    ensureCorrectIcon(icon: IconSpec) {
-        if (!icon.kind) {
-            icon.kind = "fa";
-        }
-    }
 }
 
-function gracefullyAlert(message: string) {
-    new Notice("Typing: " + message);
-    console.log("Typing: " + message);
-}
+export let registry = new TypeRegistry();
