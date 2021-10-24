d94b050914c4d46031878dd6c9eb0e4c776cdd8a
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-10-24 20:17:38 +0300

    move function hideInlineFields to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { Notice } from "obsidian";
+import { Notice, MarkdownPostProcessorContext } from "obsidian";
 
 export function getFrontmatterLengthInLines(body: string) {
     let yamlSymbol = "---";
@@ -26,3 +26,34 @@ export function gracefullyAlert(message: string) {
     new Notice("Typing: " + message);
     console.log("Typing: " + message);
 }
+
+export const hideInlineFields = (
+    el: HTMLElement,
+    ctx: MarkdownPostProcessorContext
+) => {
+    let parNodes = el.querySelectorAll("p");
+    for (let i = 0; i < parNodes.length; i++) {
+        let par = parNodes[i];
+        let parChildren = par.childNodes;
+        let childrenToRemove: Array<Node> = [];
+        for (let j = 0; j < parChildren.length; j++) {
+            let child = parChildren[j];
+            if (
+                child.nodeType == 3 &&
+                child.textContent.match(
+                    /^\s*[0-9\w\p{Letter}][-0-9\w\p{Letter}]*\s*::/u
+                )
+            ) {
+                for (let k = j; k < parChildren.length; k++) {
+                    childrenToRemove.push(parChildren[k]);
+                    if (parChildren[k].nodeName == "BR") {
+                        break;
+                    }
+                }
+            }
+        }
+        for (let child of childrenToRemove) {
+            par.removeChild(child);
+        }
+    }
+};
