fbbf3044ff21c8f88e31312520d7f019d8a4d86c
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2022-01-09 23:04:40 +0300

    extend global context

--- src/context.ts
+++ src/context.ts
@@ -1,15 +1,38 @@
-import { App } from "obsidian";
+import { App, MarkdownView } from "obsidian";
+import { DataviewApi } from "obsidian-dataview";
 import TypingPlugin from "./main";
+import { Note } from "./typing/note";
+import { Registry } from "./typing/registry";
 
-class GlobalContext {
+export class GlobalContext {
     app: App;
     plugin: TypingPlugin;
+    registry: Registry;
+    prism: any;
+
+    get dv(): DataviewApi {
+        return this.app.plugins.plugins.dataview.api;
+    }
+    get currentNote(): Note | null {
+        let view = this.app.workspace.getActiveViewOfType(MarkdownView);
+        if (!view) {
+            return null;
+        }
+        let note = new Note(view.file.path);
+        return note;
+    }
 
     setApp(app: App) {
         this.app = app;
     }
     setPlugin(plugin: TypingPlugin) {
         this.plugin = plugin;
+    }
+    setRegistry(registry: Registry) {
+        this.registry = registry;
+    }
+    setPrism(prism: any) {
+        this.prism = prism;
     }
 }
 
