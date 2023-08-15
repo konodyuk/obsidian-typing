b0f24f177a64ca0bd229ea4fdfdf379c77a8aee4
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2023-08-15 21:02:03 +0400

    fix homepage title

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -48,7 +48,7 @@ function HomepageHeader() {
 export default function Home() {
     const { siteConfig } = useDocusaurusContext();
     return (
-        <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+        <Layout title={`Home`}>
             <HomepageHeader />
             <main>
                 <div class="hero hero--dark">
